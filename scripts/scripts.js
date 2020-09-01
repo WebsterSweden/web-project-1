3ab8a208fe7ca418de33a5ecebb4e6a6ebe85b67
WebsterSweden <webster@webster.se>
2020-09-01 19:31:16 +0200

    Edit scripts.js.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,7 @@
-/*jslint devel */
+/*jslint
+browser, devel
+*/
 
 // bitwise, browser, convert, couch, devel, eval, for, fudge, getset, long, node, single, this, white
 
-console.log("scripts.js");
+console.log(document.currentScript);
