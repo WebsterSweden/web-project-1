fe73ab30078a4bc1f11ad9189c7ca6dd5393ab1a
WebsterSweden <webster@webster.se>
2020-10-29 19:18:46 +0100

    Create Image Directory. Edit Base64 image.

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,10 @@
 /*jslint
-browser, devel, es6
+browser, devel, es6, for
 */
 /*global
 window
 */
 
-// bitwise, browser, convert, couch, devel, es6, eval, for, fudge, getset, long, node, single, this, white
+// bitwise, browser, convert, couch, devel, es6, eval, fudge, getset, long, node, single, this, white
 
 console.log(document.currentScript);
